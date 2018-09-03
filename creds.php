b3266986be8667d3b6d9a8eca702eab3d0bfd34e
Alysha17 <33760557+Alysha17@users.noreply.github.com>
2018-09-03 14:47:16 -0700

    Adding files for reviewers and credentials for database

---
+++ creds.php
@@ -0,0 +1,19 @@
+<?php
+$host = '146.187.134.41';
+$db   = 'researchGrant';
+$user = 'root';
+$pass = '*b37h-M3p&T';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
